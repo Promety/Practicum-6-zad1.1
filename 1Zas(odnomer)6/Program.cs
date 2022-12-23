ed921ed2975c19e90fc844e60b3e8ef4df85269d
user <user@WIN-349MCH6R7UR>
2022-12-23 17:33:35 +0300

    Добавьте файлы проекта.

---
+++ 1Zas(odnomer)6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1Zas_odnomer_6
+{
+    class Program
+    {
+        static int[] Input(out int n)
+        {
+            
+            Console.WriteLine("Сколько элементов массива ? : ");
+            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                Console.Write("Ошибка, Введите Кол-во элементов заново\n");
+            int[] G = new int[n];
+            Console.WriteLine("Теперь, заполните массив числами :");
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("[{0}]= ", j);
+                while (!int.TryParse(Console.ReadLine(), out G[j]))
+                {
+                    Console.Write("Ошибка" + $"Введите заново [{j}]: ");
+                }
+             }
+
+            int sum = 0;
+            double count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                sum += G[j];
+                count++;
+            }
+            Console.WriteLine("Среднееарифметическое будет : " + sum / count);
+            Console.ReadKey();
+            return G;
+        }
+        static void Main(string[] args)
+        {
+            int n;           
+            int[] myArray = Input(out n);
+        }
+    }
+    
+}
